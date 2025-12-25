01c599b7cd8607365cc000d5b6a2acf25b73214b
kirankattii <kirankatti550@gmail.com>
2025-12-25 19:46:58 +0530

    resolve the error

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-
-  allowedDevOrigins: ['http://192.168.0.107:3000/', 'http://localhost:3001'],
+  // production-safe config only
 };
 
 export default nextConfig;
